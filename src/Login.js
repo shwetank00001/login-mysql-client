37a2f198448e14035e7de251df6a391d016460bd
shwetank00001 <mshwetank@gmail.com>
2023-09-14 13:40:38 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Validation from './LoginValidation'
+
+
+const Login = () => {
+
+    const [ values, setValues ] = React.useState({
+        email:"",
+        password:""
+    })
+
+    const [ error, setError ]= React.useState({})
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(values)
+        setError(Validation(values))
+    }
+
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setValues(function(item){
+            return{
+                ...item,
+                [name]: value
+            }
+        })
+    }
+  return (
+    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+        <div className='bg-white p-3 rounded w-25'>
+            <h2>Log In</h2>
+            <form action='' onSubmit={handleSubmit}>
+                <div className='mb-3'>
+                    <label htmlFor='email'>Email</label>
+                    <input id='email' type='email' name="email" value={values.email} placeholder='enter email' className='form-control rounded-0' onChange={handleChange} />
+                    {error.email && <span className='text-danger'>{error.email }</span> }
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='password'>Password</label>
+                    <input id='password' type='password' name='password' value={values.password} placeholder='enter Password' className='form-control rounded-0' onChange={handleChange} />
+                    {error.password && <span className='text-danger'>{error.password }</span> }
+                </div>
+
+                <button type='submit' className='btn btn-success w-100'>Log in</button>
+
+                <p>Must agree to our terms and policies</p>
+                <Link to="/signup" className='btn btn-default border w-100 text-decoration-none'>Create Account</Link>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/LoginValidation.js
@@ -0,0 +1,30 @@
+export default function Validation(values){
+    let error = {}
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+
+
+    if(values.email===""){
+        error.email = "Email should not be empty"
+    }
+
+    else if(!email_pattern.test(values.email)){
+        error.email = "Email didnt Match"
+    }else{
+        error.email = ""
+    }
+    if(values.password===""){
+        error.password = "Enter the field"
+    }
+
+    else if(!password_pattern.test(values.password)){
+        error.email = "Password didnt Match"
+    }else{
+        error.email = ""
+    }
+
+    return error;
+
+
+
+}
